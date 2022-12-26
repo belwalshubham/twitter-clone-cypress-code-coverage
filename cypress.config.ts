2026efeebaae3552dbe8d8e712b1460c3e4540c8
Shubham Belwal <belwalshubham08@gmail.com>
2022-12-26 17:31:34 +0530

    port 3000

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'x83arq',
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       return config
